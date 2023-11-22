741b16844881e79c887037d73966dba3a5cc3b4b
Fishskin713 <131266732+Fishskin713@users.noreply.github.com>
2023-11-22 22:10:02 +0800

    Update DevsecopsPracticeApplication.java
    html

--- src/main/java/com/example/devsecops_practice/DevsecopsPracticeApplication.java
+++ src/main/java/com/example/devsecops_practice/DevsecopsPracticeApplication.java
@@ -2,10 +2,16 @@ package com.example.devsecops_practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController; 
 
+@RestController
 @SpringBootApplication
 public class DevsecopsPracticeApplication {
-
+	@GetMapping("/")
+	public String hello() {
+		return "<html><body><h1>Hello DevSecOps</body></h1></html>";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DevsecopsPracticeApplication.class, args);
 	}
