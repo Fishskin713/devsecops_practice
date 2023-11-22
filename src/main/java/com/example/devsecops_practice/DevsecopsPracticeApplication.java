806226a29bfee6c04bb0d7d8d4464a7154610fa5
Fishskin713 <yiting600407@gmail.com>
2023-11-22 21:54:31 +0800

    v.1
    init

---
+++ src/main/java/com/example/devsecops_practice/DevsecopsPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.devsecops_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsecopsPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsecopsPracticeApplication.class, args);
+	}
+
+}
